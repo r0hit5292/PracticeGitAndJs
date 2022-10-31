d62050c1707a467b715a5d09aee7ef0303784b5b
Rohit Sachan <rohit.sachan@vidyamantra.com>
2022-10-31 13:15:02 +0530

    2nd answer

---
+++ 2ndQuestion.js
@@ -0,0 +1,21 @@
+function SplitString(str){
+    var Arrayans=[];
+    var k ="";
+    for(var i =0;i<=str.length;i++){
+       
+        if(str.charAt(i)===","||str.length==i){
+           Arrayans.push(k);
+           k="";
+        }
+        else{
+             
+            k = k+str.charAt(i);
+        
+        }
+    }
+    return Arrayans;
+    }
+    var names = 'rohit,sumit,smit,astitva';
+    var a=SplitString(names);
+    console.log(a);
+    
